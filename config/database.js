3ef7f4d14d4aa7562858838bc7b4d9f69521209e
Tiago Kontarski <tiagokontarski@gmail.com>
2023-07-30 19:00:20 -0300

    changed the db settings

--- config/database.js
+++ config/database.js
@@ -6,13 +6,13 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: 'postgres'
+        dialect: 'mysql'
     },
     test: {
         username: process.env.DB_USER_TEST,
         password: process.env.DB_PASSWORD_TEST,
         database: process.env.DB_NAME_TEST,
         host: process.env.DB_HOST_TEST,
-        dialect: 'postgres'
+        dialect: 'mysql'
     }
 }
