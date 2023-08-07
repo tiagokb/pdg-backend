d1c5f18d7a49df99b01c7fcd3b48cf9f32e8b68b
Tiago Kontarski <tiagokontarski@gmail.com>
2023-08-07 00:18:47 -0300

    Hotfix/notification route crash (#10)
    * update master with dev (#9)
    
    * added quantity on pokemons captured list
    
    * firebase admin config
    
    * notification system
    
    * remove old routes
    
    remove old routes from routes/notification.js

--- routes/notification.js
+++ routes/notification.js
@@ -5,7 +5,6 @@ const auth = require('../middlewares/auth');
 
 router.use(auth.verifyToken);
 
-router.get('/sendTestMessage', messagingController.sendTestMessage);
 router.post('/registerToken', messagingController.registerToken);
 
 module.exports = router;
