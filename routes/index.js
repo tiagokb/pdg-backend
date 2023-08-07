e40478872d5dbd2c96e942cb78d2a7af37774ae1
Tiago Kontarski <tiagokontarski@gmail.com>
2023-08-07 20:11:22 -0300

    set cors policy (#13)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 
+router.use(cors());
 router.use(express.json());
 
 // Import individuals route files
