1e7814addbb0eeae12478f557e8e3f2b221fd76b
Tiago Kontarski <tiagokontarski@gmail.com>
2023-08-18 20:08:14 -0300

    hotfix login route method

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
-router.get('/login', userController.login);
+router.post('/login', userController.login);
 
 module.exports = router;
